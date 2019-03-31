5ff1aebd34f511ba68e9a601bf67026235745f02
FreddyT53 <freddyt5371@gmail.com>
2019-03-31 10:46:32 -0700

    added images

--- src/App.js
+++ src/App.js
@@ -1,39 +1,130 @@
 import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Item from './components/Item';
+import teams from './teams.json';
+import './App.css';
+
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+  constructor() {
+    super()
+
+    this.handleShuffleTeams = this.handleShuffleTeams.bind(this)
+  }
+
   state = {
-    friends
+    score: 0,
+    topScore: 0,
+    maxScore: 12,
+    message: "Click an image to begin!",
+    messageClass: "",
+    teams: teams
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+  shuffle = (array) => {
+    let currentIndex = array.lenght;
+    let temporaryValue;
+    let randomIndex;
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  handleCorrectSelection = () => {
+    if (this.state.score + 1 > this.state.topScore) {
+      this.setState({ topScore: this.state.topScore + 1 })
+    }
+    if (this.state.score + 1 === this.state.maxScore) {
+      this.setState({ score: this.state.score + 1, message: 'Congrats! You win!', messageClass: "correct" })
+    } else {
+
+      this.setState({ score: this.state.score + 1, message: 'You guessed correctly!', messageClass: 'correct' })
+    }
+  }
+
+
+  handleResetWin = (currentTeams) => {
+    if (this.state.score + 1 === this.state.maxScore) {
+      this.setState({ score: 0, topScore: 0 })
+      const updatedTeams = currentTeams.map(team => (true) ? { ...team, isClicked: false } : team)
+      return updatedTeams
+    } else {
+      return currentTeams
+    }
+  }
+
+  handleIncorrectSelection = () => {
+    this.setState({ score: 0, message: 'You guessed incorrectly!' })
+    const updatedTeams = this.state.teams.map(teams => teams.isClicked === true ? { ...teams, isClicked: false } : teams)
+    return updatedTeams
+  }
+
+  handleShuffleTeams = (name) => {
+    var resetNeeded = false;
+    const teams = this.state.teams.map(teams => {
+      if (teams.name === name) {
+        if (teams.isClicked === false) {
+          this.handleCorrectSelection()
+          return { ...teams, isClicked: true }
+        } else {
+          resetNeeded = true
+          return { ...teams, isClicked: false }
+        }
+      }
+      return teams
+    })
+    if (resetNeeded) {
+      this.setState({
+        teams: this.shuffle(this.handleIncorrectSelection()),
+        messageClass: 'incorrect'
+      })
+    } else {
+      this.setState({ teams: this.shuffle(this.handleResetWin(teams)) })
+    }
+  }
+
+  handleRenderTeams = () => {
+    return this.state.teams.map((teams) =>
+      <Item
+        image={teams.image}
+        name={teams.name}
+        key={teams.id}
+        onClick={this.handleShuffleTeams}
+      />
+    );
+  }
+
   render() {
     return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-          />
-        ))}
-      </Wrapper>
+      <div className='App'>
+        <Navbar
+          score={this.state.score}
+          topScore={this.state.topScore}
+          message={this.state.message}
+          messageClass={this.state.messageClass}
+        />
+
+
+        <Header />
+        <div className='content'>
+          {this.handleRenderTeams()}
+        </div>
+        <Footer />
+      </div>
     );
   }
 }
+
+
 
 export default App;
---
+++ src/components/Content/Content.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './Content.css'
+
+const Content = () => (
+    <div className='content'/>
+);
+
+export default Content;
